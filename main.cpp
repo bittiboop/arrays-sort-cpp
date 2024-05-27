d4a680442479fa3e465cffb791e98a0f8b392f28
bittiboop <sstrelkova15@gmail.com>
2024-05-27 19:46:25 +0300

    added code ex 4

--- main.cpp
+++ main.cpp
@@ -1,31 +1,18 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 void add(int num);
 
 int main() {
 	int num;
-	cout << "Enter the number to know prime its or not: ";
+	cout << "Enter the number to know the cube of it: ";
 	cin >> num;
 	add(num);
 
 	return 0;
 }
 void add(int num) {
-	bool isPrime = true;
-	for (int i = 2; i <= num / 2; i++) {
-		if (num % i == 0) {
-			isPrime = false;
-			break;
-		}
-	}
-	if (num == 0 || num == 1)
-	{
-		isPrime = false;
-	}
-	if(isPrime)
-		cout << num << " is a prime number." << endl;
-	else
-		cout << num << " is not a prime number." << endl;
+	cout << pow(num, 3);
 	return;
 }
