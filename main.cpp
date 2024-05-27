1fd4d6bd70394dcec77d8fdeff4dce7681a97cce
bittiboop <sstrelkova15@gmail.com>
2024-05-27 20:46:56 +0300

    added base code

--- main.cpp
+++ main.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
-void add(int strt, int end) {
-	int perf = 0;
-	for (int i = strt; i <= end; i++) {
-		if (i == 1) {
-			continue;
+void add(int cardind) {
+	cin >> cardind;
+	if (cardind > 0 && cardind < 11) {
+		cout << char(218);
+		for (int i = 0; i < 20; i++) {
+			cout << char(196);
 		}
-		if (i == 6 || i == 28 || i == 496 || i == 8128 || i == 33550336) {
-			perf++;
-			cout << i << endl;
+		cout << char(191) << endl;
+		cout << char(179) << cardind;
+		for (int i = 0; i < 10; i++) {
+			cout << "-" << setw(2);
 		}
+		cout << char(179) << endl;
+		}
+	else {
+		cout << "Your`e kidding me, right?";
 	}
-	cout << "The perfect numbers between " << strt << " and " << end << " are: " << perf << endl;
 	return;
 }
 int main() {
-	int strt, end;
-	cout << "Enter the start of the range: ";
-	cin >> strt;
-	cout << "Enter the end of the range: ";
-	cin >> end;
-	add(strt, end);
+	int cardind;
+	cout << "Enter the card: ";
+	cin >> cardind;
+	add(cardind);
 	
 	return 0;
 }
