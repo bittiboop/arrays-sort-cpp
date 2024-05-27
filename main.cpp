ae08119f5e2ab1a6a08e4e7afd386867b751723f
bittiboop <sstrelkova15@gmail.com>
2024-05-27 19:00:04 +0300

    added ex 2

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
-int add(int height, int width) {
-	for (size_t i = 0; i < height; i++) {
-		for (size_t j = 0; j < width; j++) {
-			cout << "*";
-		}
-		cout << endl;
-	}
-	return 0;
-}
+
+int factorial(int factnum);
+
 int main() {
-	int height, width;
-	cout << "Enter the height and width of the rectangle: ";
-	cin >> height >> width;
-	cout << add(height, width);
+	int factnum;
+	cout << "Enter the number from what you want to get factorial: ";
+	cin >> factnum;
+	cout << factnum << "! is: " << factorial(factnum) << endl;
 
 	return 0;
+}
+int factorial(int factnum) {
+	if(factnum > 1)
+		return factnum * factorial(factnum - 1);
+	else if(factnum == 1)
+		return 1;
+	else if(factnum == 0)
+		return 0;
 }
