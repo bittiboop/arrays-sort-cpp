ecbebdbfdd8be5ffa4d311e45f196f6ff2dca323
bittiboop <sstrelkova15@gmail.com>
2024-05-27 19:58:46 +0300

    made ex 6

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 using namespace std;
-int add(int height, int width) {
-	for (size_t i = 0; i < height; i++) {
-		for (size_t j = 0; j < width; j++) {
-			cout << "*";
-		}
-		cout << endl;
+
+void add(int num) {
+	if (num < 0)
+	{
+		cout << "False!";
 	}
-	return 0;
+	if (num >= 0)
+	{
+		cout << "True!";
+	}
+	return;
 }
 int main() {
-	int height, width;
-	cout << "Enter the height and width of the rectangle: ";
-	cin >> height >> width;
-	cout << add(height, width);
+	int num;
+	cout << "Enter the number: ";
+	cin >> num;
+	add(num);
 	
 	return 0;
 }
